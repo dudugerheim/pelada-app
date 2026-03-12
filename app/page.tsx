83ebf45beb398fe9779e1dbbcb6b2f3cc71421bd
Eduardo Gerheim <dudugerheim@gmail.com>
2026-03-11 21:10:24 -0300

    Corrigido redirect da raiz para Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-// app/page.tsx
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  // Redireciona automaticamente para a Home
-  redirect("/home"); // se quiser, pode trocar para "/gerar-times"
+  redirect("/home");
 }
